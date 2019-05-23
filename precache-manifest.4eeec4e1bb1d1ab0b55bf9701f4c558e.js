0be95b8eb3a231fa2266ec40440c5cf50b19e9e5
wumin <wu.min@qianka.com>
2019-05-23 15:11:24 +0800

    deploy

---
+++ precache-manifest.4eeec4e1bb1d1ab0b55bf9701f4c558e.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ecb4b1f51572913db53",
+    "url": "/huan-web/js/chunk-vendors.b76ed3a0.js"
+  },
+  {
+    "revision": "301e454bef6bcbcb328c8b76d429ce90",
+    "url": "/huan-web/location.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/huan-web/robots.txt"
+  },
+  {
+    "revision": "936d6e411cabd71f0e627011c3f18fe2",
+    "url": "/huan-web/img/icons_deprecated/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "4076ebe5db4c480a62fa",
+    "url": "/huan-web/js/about.44ceae47.js"
+  },
+  {
+    "revision": "198eb557c37ef8e357fe",
+    "url": "/huan-web/js/app.accd8a86.js"
+  },
+  {
+    "revision": "11cfd569ab1cfa2ca40b",
+    "url": "/huan-web/js/chunk-19316604.31f0030f.js"
+  },
+  {
+    "revision": "38615240f6826ac16ef6",
+    "url": "/huan-web/js/chunk-118d0910.ff63491b.js"
+  },
+  {
+    "revision": "79aca786d214bdecc97d",
+    "url": "/huan-web/js/chunk-1b5fd064.96b2ab10.js"
+  },
+  {
+    "revision": "ae8f855e10248a394d3b",
+    "url": "/huan-web/js/chunk-6044b218.68ec5b68.js"
+  },
+  {
+    "revision": "e0f3d4d948943c270d1b",
+    "url": "/huan-web/js/chunk-43ab35e3.9c053fd4.js"
+  },
+  {
+    "revision": "82603b2797377dcc8dd7",
+    "url": "/huan-web/js/chunk-5be8276c.7bf63066.js"
+  },
+  {
+    "revision": "c43cd371a49ee4ca17ab3a60e72bdd51",
+    "url": "/huan-web/img/icons_deprecated/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "82603b2797377dcc8dd7",
+    "url": "/huan-web/css/chunk-5be8276c.c02a1d9b.css"
+  },
+  {
+    "revision": "f22d501a35a87d9f21701cb031f6ea17",
+    "url": "/huan-web/img/icons_deprecated/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "1ecb4b1f51572913db53",
+    "url": "/huan-web/css/chunk-vendors.4f943e57.css"
+  },
+  {
+    "revision": "058a3335d15a3eb84e7ae3707ba09620",
+    "url": "/huan-web/img/icons_deprecated/mstile-150x150.png"
+  },
+  {
+    "revision": "c52069fdf635dbc7894ec591a9db2d41",
+    "url": "/huan-web/index.html"
+  },
+  {
+    "revision": "b89032a4a5a1879f30ba05a13947f26f",
+    "url": "/huan-web/img/icons_deprecated/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "f130a0b70e386170cf6f011c0ca8c4f4",
+    "url": "/huan-web/img/icons_deprecated/android-chrome-192x192.png"
+  },
+  {
+    "revision": "0ff1bc4d14e5c9abcacba7c600d97814",
+    "url": "/huan-web/img/icons_deprecated/android-chrome-512x512.png"
+  },
+  {
+    "revision": "9a2b5c0f19de617685b7b5b42464e7db",
+    "url": "/huan-web/img/icons_deprecated/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "ae8f855e10248a394d3b",
+    "url": "/huan-web/css/chunk-6044b218.7a38d05e.css"
+  },
+  {
+    "revision": "1a034e64d80905128113e5272a5ab95e",
+    "url": "/huan-web/img/icons_deprecated/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "4bb1a55479d61843b89a2fdafa7849b3",
+    "url": "/huan-web/img/icons_deprecated/favicon-16x16.png"
+  },
+  {
+    "revision": "af28d69d59284dd202aa55e57227b11b",
+    "url": "/huan-web/img/icons_deprecated/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "66830ea6be8e7e94fb55df9f7b778f2e",
+    "url": "/huan-web/img/icons_deprecated/apple-touch-icon.png"
+  },
+  {
+    "revision": "98b614336d9a12cb3f7bedb001da6fca",
+    "url": "/huan-web/img/icons_deprecated/favicon-32x32.png"
+  },
+  {
+    "revision": "e0f3d4d948943c270d1b",
+    "url": "/huan-web/css/chunk-43ab35e3.dbea8aa2.css"
+  },
+  {
+    "revision": "79aca786d214bdecc97d",
+    "url": "/huan-web/css/chunk-1b5fd064.f86e8939.css"
+  },
+  {
+    "revision": "11cfd569ab1cfa2ca40b",
+    "url": "/huan-web/css/chunk-19316604.9fdf6fbd.css"
+  },
+  {
+    "revision": "38615240f6826ac16ef6",
+    "url": "/huan-web/css/chunk-118d0910.a2caf6a0.css"
+  },
+  {
+    "revision": "198eb557c37ef8e357fe",
+    "url": "/huan-web/css/app.9453afb6.css"
+  }
+];
